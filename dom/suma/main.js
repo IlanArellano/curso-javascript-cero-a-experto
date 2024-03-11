9ce371b8de5ab4946bb8b151fd63ee5c80d34c52
Ilan Arellano <72232853+IlanArellano@users.noreply.github.com>
2024-03-10 19:54:28 -0600

    inicio

---
+++ dom/suma/main.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("#form_suma")
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault()
+    const numero_1 = +form.numero_1.value
+    const numero_2 = +form.numero_2.value
+    const suma = numero_1 + numero_2
+    const etiqueta_resultado = document.querySelector("#resultado")
+    etiqueta_resultado.innerHTML = "El resultado es: " + suma
+})
